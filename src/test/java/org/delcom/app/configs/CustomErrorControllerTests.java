8c90ff80ab6e67b8ccef034a7589bf7ae18a2d64
estermelatii <manurungester477@gmail.com>
2025-11-14 23:15:00 +0700

    FIX

--- src/test/java/org/delcom/app/configs/CustomErrorControllerTests.java
+++ src/test/java/org/delcom/app/configs/CustomErrorControllerTests.java
@@ -3,6 +3,7 @@ package org.delcom.app.configs;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.webmvc.error.ErrorAttributes;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomErrorControllerTest {
 
         @Test
@@ -62,6 +64,7 @@ class CustomErrorControllerTest {
 
                 CustomErrorController controller = new CustomErrorController(errorAttributes);
 
+                // buat dummy request/response
                 HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
                 HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
                 ServletWebRequest webRequest = new ServletWebRequest(request, response);
